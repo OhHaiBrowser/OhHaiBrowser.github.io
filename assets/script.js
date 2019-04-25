66c95d56f25e38be1895f64ddad3a81c065a1837
Lucas <Lucas.Reade@yahoo.com>
2019-04-25 17:32:20 +0100

    Current OhHaiBrowser website

---
+++ assets/script.js
@@ -0,0 +1,30 @@
+var MainDownloadBtn = document.getElementById("MainDownloadBtn");
+var DownloadBtn = document.getElementById("DownloadBtn");
+
+switch(Detect.OS){
+	//case "macOS":
+	//	DownloadBtn.href = "assets/apps/";
+	//	DownloadBtn.textContent = "Download for Mac"
+	//	DownloadBtn.classList.remove("isDisabled");
+	//break;
+	//case "Linux":
+	//	DownloadBtn.href = "assets/apps/";
+	//	DownloadBtn.textContent = "Download for Linux"
+	//	DownloadBtn.classList.remove("isDisabled");
+	//break;
+	case "Windows":
+		MainDownloadBtn.href = "assets/apps/OhHaiBrowser-windows-setup.exe";
+		MainDownloadBtn.textContent = "Download for Windows"
+		MainDownloadBtn.classList.remove("isDisabled");
+	
+		DownloadBtn.href = "assets/apps/OhHaiBrowser-windows-setup.exe";
+		DownloadBtn.textContent = "Download for Windows"
+		DownloadBtn.classList.remove("isDisabled");
+	break;
+	default:
+		MainDownloadBtn.href = ""
+		MainDownloadBtn.textContent = "Not yet supported on your OS"
+	
+		DownloadBtn.href = ""
+		DownloadBtn.textContent = "Not yet supported on your OS"	
+}
